177ae59949c1cde797b363762e4bf51619ac81d7
John Cooper <john@choffee.co.uk>
2012-04-30 08:21:59 +0100

    Fixed some apache requirements

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -9,25 +9,27 @@ define graphite::apache {
   $servername = $title
   # Install
   package {['apache2',
-            'libapache2-mod-wsgi'
-           ]: 
+            'libapache2-mod-wsgi']:
     ensure => installed,
   }
   # Config
   file {'/etc/apache2/mods-enabled/wsgi.conf':
-    ensure => link,
-    target => '/etc/apache2/mods-available/wsgi.conf',
-    notify => Service['apache2'],
+    ensure  => link,
+    target  => '/etc/apache2/mods-available/wsgi.conf',
+    notify  => Service['apache2'],
+    require => Package['libapache2-mod-wsgi']
   }
   file {'/etc/apache2/mods-enabled/wsgi.load':
-    ensure => link,
-    target => '/etc/apache2/mods-available/wsgi.load',
-    notify => Service['apache2'],
+    ensure  => link,
+    target  => '/etc/apache2/mods-available/wsgi.load',
+    notify  => Service['apache2'],
+    require => Package['libapache2-mod-wsgi']
   }
   file {'/etc/apache2/sites-enabled/graphite.conf':
-    ensure => link,
-    target => '/etc/apache2/sites-available/graphite.conf',
-    notify => Service['apache2'],
+    ensure  => link,
+    target  => '/etc/apache2/sites-available/graphite.conf',
+    notify  => Service['apache2'],
+    require => Package['apache2']
   }
   file {'/etc/apache2/sites-available/graphite.conf':
     content => template('graphite/apache/graphite.conf.erb'),
@@ -35,21 +37,25 @@ define graphite::apache {
     group   => root,
     mode    => '0444',
     notify  => Service['apache2'],
+    require => Package['apache2']
   }
   file {"${instdir}/storage/log/webapp":
-    owner => 'www-data',
-    group => 'www-data',
+    owner   => 'www-data',
+    group   => 'www-data',
+    require => Package['apache2']
   }
   file {"${instdir}/storage/index":
-    ensure => present,
-    owner  => 'www-data',
-    group  => 'www-data',
-    mode   => '0644',
+    ensure  => present,
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0644',
+    require => Package['apache2']
   }
 
   # Service
   service { 'apache2':
-    ensure => running
+    ensure  => running
+    require => Package['apache2']
   }
 
 
